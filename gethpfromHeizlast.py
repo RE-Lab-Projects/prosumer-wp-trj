727f6829f18c87ae9cf3e50921e6e16e729af7b6
Hauke Hoops <75681123+xihaui@users.noreply.github.com>
2022-07-02 14:56:37 +0200

    Anpassung des Tww Aufschlags

--- gethpfromHeizlast.py
+++ gethpfromHeizlast.py
@@ -17,21 +17,21 @@ def fitting_hp(energieverbrauch, standort,Vorlauftemperatur,Baujahr,Personen, ef
     
     hp=pd.read_csv('src/simulation_data/hp_Normheizlast.csv')
     weather=pd.read_csv('src/simulation_data/TRJ-Tabelle.csv').head(15)# average year
-    eff_heiz=0.9                #average from DIN EN 12831 Tabelle 38
-    if Baujahr<=2000:           
-        if Baujahr<=1995:
-            eff_heiz=0.85       #average from DIN EN 12831 Tabelle 38
-        Heizgrenztemperatur=15  #IWU Heizgrenztemperatur 
-        gtz=weather.iloc[standort-1,11]
-    elif Baujahr>2015:
-        Heizgrenztemperatur=10
-        gtz=weather.iloc[standort-1,12]
-    else:
-        Heizgrenztemperatur=12
-        gtz=weather.iloc[standort-1,10]
-    b=gtz*24/(Heizgrenztemperatur-weather.iloc[standort-1,9])   #DIN/TS 12831-1:2020-04 Formel 50
-    Q_TWW=(14.9*30*Personen)/eff_tww                            #DIN/TS 12831-1:2020-04 Formel 57
-    Heizlast = (energieverbrauch-Q_TWW)* eff_heiz * 1000 / b    #DIN/TS 12831-1:2020-04 Formel 49
-    Heizbedarf=Heizlast+200*Personen                            # Aufschlag TWW
+    eff_heiz=0.9                                                    # average from DIN EN 12831 Tabelle 38
+    if Baujahr<=2000:                                                
+        if Baujahr<=1995:                                    
+            eff_heiz=0.85                                           # average from DIN EN 12831 Tabelle 38
+        Heizgrenztemperatur=15                                      # IWU Heizgrenztemperatur 
+        gtz=weather.iloc[standort-1,11] 
+    elif Baujahr>2015: 
+        Heizgrenztemperatur=10 
+        gtz=weather.iloc[standort-1,12] 
+    else: 
+        Heizgrenztemperatur=12 
+        gtz=weather.iloc[standort-1,10] 
+    b=gtz*24/(Heizgrenztemperatur-weather.iloc[standort-1,9])       # DIN/TS 12831-1:2020-04 Formel 50
+    Q_TWW=(14.9*30*Personen)/eff_tww                                # DIN/TS 12831-1:2020-04 Formel 57
+    Heizlast = (energieverbrauch* eff_heiz-Q_TWW) * 1000 / b        # DIN/TS 12831-1:2020-04 Formel 49
+    Heizbedarf=Heizlast+Q_TWW                                       # Aufschlag TWW (500h im Jahr (1.5h am Tag))
     hp=hp.loc[(hp['Standort']==standort)& (hp['Vorlauftemperatur']==Vorlauftemperatur)&(hp['Normheizlast']>=Heizbedarf)&(hp['Normheizlast']<=Heizbedarf*1.25)]
-    return hp.sort_values('COP', ascending=False), Heizbedarf
+    return hp.sort_values('Normheizlast', ascending=False), Heizbedarf
